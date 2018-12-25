b680755b564e4a2b5a56739af02ae69b2c20e3fd
髙橋 佑太 <yuta.takahashi@jbs.com>
2018-12-25 17:52:32 +0900

    feature: Render TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import './App.css';
 import AddTodoForm from './containers/AddTodoForm/AddTodoForm';
+import TodoList from './containers/TodoList/TodoList';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
+      <div>
         <AddTodoForm />
+        <TodoList />
       </div>
     );
   }
