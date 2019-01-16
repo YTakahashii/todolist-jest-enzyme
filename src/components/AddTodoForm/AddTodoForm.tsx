1b753270aee8bd9e4455bc04067b6293efed4132
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-01-16 16:29:52 +0900

    fix: private to public button's className

--- src/components/AddTodoForm/AddTodoForm.tsx
+++ src/components/AddTodoForm/AddTodoForm.tsx
@@ -3,49 +3,51 @@ import AddTodoFormProps from './AddTodoFromProps';
 import AddTodoFormState from './AddTodoFromState';
 
 export default class AddTodoForm extends React.Component<
-  AddTodoFormProps,
-  AddTodoFormState
+    AddTodoFormProps,
+    AddTodoFormState
 > {
-  constructor(props: AddTodoFormProps) {
-    super(props);
+    constructor(props: AddTodoFormProps) {
+        super(props);
 
-    this.state = {
-      input: ''
-    };
-  }
-
-  public render() {
-    return (
-      <div>
-        <form onSubmit={this.onFormSubmit}>
-          <input
-            type="text"
-            value={this.state.input}
-            onChange={this.onInputChange}
-          />
-          <button type="submit">Todoを追加</button>
-        </form>
-      </div>
-    );
-  }
-
-  private onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!this.state.input.trim()) {
-      return;
+        this.state = {
+            input: ''
+        };
     }
 
-    this.props.addTodo(this.state.input);
+    public render() {
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                    <input
+                        type='text'
+                        value={this.state.input}
+                        onChange={this.onInputChange}
+                    />
+                    <button type='submit' className='todo-submit'>
+                        Todoを追加
+                    </button>
+                </form>
+            </div>
+        );
+    }
 
-    this.setState({
-      input: ''
-    });
-  };
+    public onInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+        this.setState({
+            input: e.currentTarget.value
+        });
+    };
 
-  private onInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    this.setState({
-      input: e.currentTarget.value
-    });
-  };
+    public onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!this.state.input.trim()) {
+            return;
+        }
+
+        this.props.addTodo(this.state.input);
+
+        this.setState({
+            input: ''
+        });
+    };
 }
