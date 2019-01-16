8436f7584f32c515e313357f18be5a1387a3f2ba
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-01-16 16:26:41 +0900

    fix: import error

--- src/containers/AddTodoForm/AddTodoForm.ts
+++ src/containers/AddTodoForm/AddTodoForm.ts
@@ -1,17 +1,17 @@
 import { Dispatch } from 'redux';
-import TodoAction from 'src/actions/Todo/TodoAction';
+import TodoAction from '../../actions/Todo/TodoAction';
 import { AddTodoFormDispatchProps } from 'src/components/AddTodoForm/AddTodoFromProps';
-import { addTodo } from 'src/actions/Todo/TodoActionCreator';
+import { addTodo } from '../../actions/Todo/TodoActionCreator';
 import { connect } from 'react-redux';
-import AddTodoForm from 'src/components/AddTodoForm/AddTodoForm';
+import AddTodoForm from '../../components/AddTodoForm/AddTodoForm';
 
 const mapDispatchToProps = (
-  dispatch: Dispatch<TodoAction>
+    dispatch: Dispatch<TodoAction>
 ): AddTodoFormDispatchProps => ({
-  addTodo: (s: string) => dispatch(addTodo(s))
+    addTodo: (s: string) => dispatch(addTodo(s))
 });
 
 export default connect(
-  null,
-  mapDispatchToProps
+    null,
+    mapDispatchToProps
 )(AddTodoForm);
--- src/containers/TodoList/TodoList.ts
+++ src/containers/TodoList/TodoList.ts
@@ -1,26 +1,26 @@
 import RootState from 'src/states';
 import {
-  TodoListConnectedProps,
-  TodoListDispatchProps
+    TodoListConnectedProps,
+    TodoListDispatchProps
 } from 'src/components/TodoList/TodoListProps';
 import { Dispatch } from 'redux';
-import TodoAction from 'src/actions/Todo/TodoAction';
-import { toggleTodo, deleteTodo } from 'src/actions/Todo/TodoActionCreator';
+import TodoAction from '../../actions/Todo/TodoAction';
+import { toggleTodo, deleteTodo } from '../../actions/Todo/TodoActionCreator';
 import { connect } from 'react-redux';
-import TodoList from 'src/components/TodoList/TodoList';
+import TodoList from '../../components/TodoList/TodoList';
 
 const mapStateToProps = (state: RootState): TodoListConnectedProps => ({
-  todos: state.todos
+    todos: state.todos
 });
 
 const mapDispatchToProps = (
-  dispatch: Dispatch<TodoAction>
+    dispatch: Dispatch<TodoAction>
 ): TodoListDispatchProps => ({
-  toggleTodo: (id: number) => dispatch(toggleTodo(id)),
-  deleteTodo: (id: number) => dispatch(deleteTodo(id))
+    toggleTodo: (id: number) => dispatch(toggleTodo(id)),
+    deleteTodo: (id: number) => dispatch(deleteTodo(id))
 });
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(TodoList);
