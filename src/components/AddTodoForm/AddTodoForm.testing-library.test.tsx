ec5eccc0403bf5c1f1901aa33c6b75b1e4eb7ccb
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-02-06 01:22:08 +0900

    react-testing-libraryでユーザーストーリーのテスト実装

---
+++ src/components/AddTodoForm/AddTodoForm.testing-library.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import AddTodoForm from './AddTodoForm';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+
+afterEach(cleanup);
+
+describe('AddTodoForm(testing-lib)', () => {
+    describe('common', () => {
+        test('値を入力して「Todoを追加」ボタンを押すとaddTodoが呼び出される', () => {
+            const addTodoSpy = jest.fn();
+            const utils = render(<AddTodoForm addTodo={addTodoSpy} />);
+            const input = utils.getByLabelText(
+                'todo-input'
+            ) as HTMLInputElement;
+            const button = utils.getByText('Todoを追加') as HTMLButtonElement;
+
+            // inputに'Todo'と入力をシミュレート
+            fireEvent.change(input, { target: { value: 'Todo' } });
+            // inputのvalueに入力した値が描画される
+            expect(input.value).toEqual('Todo');
+            // Todoを追加ボタンを押す
+            fireEvent.submit(button);
+            // addTodoが発火する
+            expect(addTodoSpy).toHaveBeenCalledTimes(1); // 回数は1回
+            expect(addTodoSpy).toHaveBeenCalledWith('Todo'); // 引数はTodo
+        });
+    });
+});
